dfbd0e861880ca8f0d4ac72b3b2efeb9869d4c2a
Diya K <diyakalaria19@gmail.com>
2025-11-23 16:55:37 +0530

    Add files via upload

---
+++ post_lab_17.py
@@ -0,0 +1,15 @@
+# import sympy as sp
+# n, z, a = sp.symbols('n z a')
+# x_n = 3**n
+# X_z = sp.summation(x_n * z**(-n), (n, 0, sp.oo))
+# print("Z-transform of x[n] = a^n u[n]:")
+# sp.pprint(X_z, use_unicode=True)
+
+
+#question 2
+import sympy as sp
+n, z, omega = sp.symbols('n z omega')
+x_n = sp.cos(omega * n)
+X_z = sp.summation(x_n * z**(-n), (n, 0, sp.oo))
+print("Z-transform of x[n] = cos(omega * n) u[n]:")
+sp.pprint(X_z, use_unicode=True)
